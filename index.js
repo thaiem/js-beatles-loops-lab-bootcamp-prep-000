570a67f8becd799229e83053ebfd606e62367035
Elaine Thai <ethai1827@gmail.com>
2019-05-14 22:28:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function theBeatlesPlay(musicians, instruments) {
   var answer = [];
   for (let i = 0; i < musicians.length; i++) {
-    
+    answer.push(musicians[i] + " plays " + instruments[i]);
   }
+  return answer;
+}
+
+function johnLennonFacts(facts) {
+  
 }
